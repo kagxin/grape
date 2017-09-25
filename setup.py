ed123e4e8ea1d497d501ed9bab3d79fa8a21f29c
康鑫 <k506855660@163.com>
2017-09-25 22:56:28 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme_file = os.path.join(here, 'README.rst')
+#changes_file = os.path.join(here, 'CHANGES.rst')
+
+long_description = '\n\n'.join((
+    open(readme_file).read(),
+    open(changes_file).read(),
+))
+
+
+setup(
+    name = 'grape',
+    version = '0.0.1',
+    url = 'http://github.com/kagxin/grape',
+    license = 'BSD',
+    description = 'Remote logging support for django.',
+    long_description = long_description,
+    author = "kagxin",
+    author_email = 'kagxin at gmail dot com',
+    packages = find_packages(),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    zip_safe = True,
+    install_requires = ['setuptools', "six"],
+)
